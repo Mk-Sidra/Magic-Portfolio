a9519d2a9ac02f974cb5f22d74c9efce43044c8e
Sidra Tul Muntaha <sidra@example.com>
2026-04-20 11:38:03 +0500

    change the last name

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,9 +2,9 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Sidra",
-  lastName: "Tul Muntaha",
-  name: `Sidra Tul Muntaha`,
+  firstName: "Muntaha",
+  lastName: "Khan",
+  name: `Muntaha Khan`,
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
@@ -76,7 +76,7 @@ const home: Home = {
   },
   subline: (
     <>
-    I'm Sidra, a design engineer at <Text as="span" size="xl" weight="strong">ONCE UI</Text>, where I craft intuitive <br /> user experiences. After hours, I build my own projects.
+    I'm Muntaha, a design engineer at <Text as="span" size="xl" weight="strong">ONCE UI</Text>, where I craft intuitive <br /> user experiences. After hours, I build my own projects.
 </>
   ),
 };
@@ -102,7 +102,7 @@ const about: About = {
     title: "Introduction",
     description: (
       <>
-        Sidra is a Jakarta-based design engineer with a passion for transforming complex challenges
+        Muntaha is a Jakarta-based design engineer with a passion for transforming complex challenges
         into simple, elegant design solutions. Her work spans digital interfaces, interactive
         experiences, and the convergence of design and technology.
       </>
